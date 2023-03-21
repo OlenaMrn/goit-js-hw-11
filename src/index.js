06766220fa3e29eadefbe4295e4b2626a1d65d5b
Olena <anyanyme@gmail.com>
2023-03-21 16:55:11 +0100

    add gallery create and styles
    add gallery create and styles, use axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './css/styles.css';
-import axios from 'axios';
-import PixApiService from './pix-api-service.js'
+
+// бібліотеки
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// клас
+import PixApiService from './pix-api-service.js';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
@@ -13,17 +18,54 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 const pixApiService = new PixApiService();
 
-let searchQuery = "";
+let searchQuery = '';
 
 function onSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    pixApiService.query = e.currentTarget.elements.searchQuery.value;
-    pixApiService.resetPage ();
-pixApiService.fetchPictures(searchQuery);
-
+  pixApiService.query = e.currentTarget.elements.searchQuery.value;
+  pixApiService.resetPage();
+  pixApiService.fetchPictures().then(appendGalleryMarkup);
 }
 
 function onLoadMore() {
- pixApiService.fetchPictures(searchQuery);
- };
+  pixApiService.fetchPictures(searchQuery).then(appendGalleryMarkup);
+}
+
+function appendGalleryMarkup(hits) {
+  refs.galleryContainer.insertAdjacentHTML(
+    'beforeend',
+    createGalleryCardsMarkup(hits)
+  );
+}
+
+function createGalleryCardsMarkup(hits) {
+  return hits
+    .map(
+      hit => `
+      <div class="photo-card">
+        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${hit.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${hit.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${hit.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${hit.downloads}</b>
+          </p>
+        </div>
+      </div>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  enableKeyboard: true,
+});
--- src/pix-api-service.js
+++ src/pix-api-service.js
@@ -1,40 +1,48 @@
+import axios from 'axios';
+
+const API_KEY = '34494219-18836f66a27c5c5fdb378157c';
+
 export default class PixApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    }
-    
-    fetchPictures() {
-    // console.log(this);
-    const API_KEY = '34494219-18836f66a27c5c5fdb378157c';
-    const per_page = 40;
-    // const image_type = 'photo';
-    // const orientation = 'horizontal';
-    // const safesearch = true;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalHits = 0;
+  }
 
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchQuery}&per_page=40&page=${this.page}`;
+  async fetchPictures() {
+    const params = {
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: 40,
+    };
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-          this.page += 1;
-      })
-      .catch(error => console.log('error'));
-    }
-    
-    resetPage() {
-        this.page = 1;
+    const response = await axios.get('https://pixabay.com/api/', { params });
+    const { data } = response;
+    this.page += 1;
+    return data.hits;
+  }
 
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get hits() {
+    return this.totalHits;
+  }
+
+  set hits(newTotalHits) {
+    this.totalHits = newTotalHits;
+  }
 }
-
-    get query() {
-        return this.searchQuery;
-    }
-
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
-
-
-}
-
